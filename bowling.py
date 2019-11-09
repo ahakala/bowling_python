84ecc36c6a7472ab1f1a50d6ae4ff62fad789d2e
Hakala <andyhakala@gmail.com>
2019-11-09 15:22:29 -0600

    cleaning up strike scoring

--- bowling.py
+++ bowling.py
@@ -12,27 +12,48 @@ def score_game():
         frame +=1
     return score
 
+def spare_score(next_ball):
+    score = 10 + next_ball
+    return score
+
+def score_strike(frame_index, frames):
+    pins = 10
+    next_ball = frame_index + 1
+    next_frame_score = frames[next_ball]
+    if len(next_frame_score) == 1:
+            pins = 20
+            next_ball = frame_index + 2
+            if len(frames[next_ball]) == 1:
+                return 30
+            else:
+                total = pins + sum(next_frame_score)
+                return total
+
 def total_score(frames):
     game_score = 0
     frame_index = 0
     for frame in frames:
         while frame_index <= 9:
             if len(frame) == 1:
-                strike = 10
-                next_ball = frame_index + 1
-                next_frame_score = frames[next_ball]
-                if len(next_frame_score) == 1:
-                    total = 30
-                    game_score = game_score + total
-                    frame_index +=1
-                else:
-                    total = strike + sum(next_frame_score)
-                    game_score = game_score + total
-                    frame_index +=1
+                total = score_strike(frame_index, frames)
+                game_score = game_score + total
+                frame_index +=1
             else:
                 frame_score = sum(frame)
-                game_score = game_score + frame_score
-                frame_index +=1
+                if frame_score == 10:
+                    next_frame = frame_index + 1
+                    next_ball = frames[next_frame]
+                    print("Next ball is ", next_ball[0])
+                    total = spare_score(next_ball[0])
+                    print(total, "Frame{}".format(frame))
+                    game_score = game_score + total
+                    print(game_score)
+                    frame_index +=1
+                else:
+                    print(frame_score)
+                    game_score = game_score + frame_score
+                    print(game_score)
+                    frame_index +=1
     
     return game_score
         
--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ from bowling import score_game, total_score
 gutter_game = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
 simple_game = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
 strike_game = [[10],[10],[10],[10],[10],[10],[10],[10],[10],[10],[10],[10]]
+complex_game = [[6,1],[9,0],[8,2],[5,5],[8,0],[6,2],[9,1],[7,2],[8,2],[9,1],[7]]
+
 class GutterGame(unittest.TestCase):
 
     def test_final_score(self):
@@ -22,6 +24,12 @@ class StrikeGame(unittest.TestCase):
     def test_strike_game(self):
         final = total_score(strike_game)
         self.assertEqual(final, 300, "Should be 300")
+
+class ComplexGame(unittest.TestCase):
+
+    def test_complex_game(self):
+        final = total_score(complex_game)
+        self.assertEqual(final, 127, "Should be 127")
 
 if __name__ == '__main__':
     unittest.main()
